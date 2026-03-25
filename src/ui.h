b5efe0f864bd7a3f5363d8d6390a9e04978f7dc0
priyansh-2025 <priyansh.2025@vitstudent.ac.in>
2026-03-25 23:09:05 +0530

    Delete src directory

--- src/ui.h
+++
@@ -1,9 +0,0 @@
-#ifndef UI_H
-#define UI_H
-
-// Function prototypes for user interface
-void display_menu(void);
-int get_user_choice(void);
-void display_message(const char* message);
-
-#endif // UI_H
